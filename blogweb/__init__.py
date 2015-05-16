a5cc9c983e9edcb2321937571c52943b00ae14fe
tanabe_taichi <whpbj809@gmail.com>
2015-05-16 23:18:05 +0900

    add some routes; add the definition of Article and AdminUser

--- blogweb/__init__.py
+++ blogweb/__init__.py
@@ -13,9 +13,27 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+
     config = Configurator(settings=settings)
+
     config.include('pyramid_chameleon')
+
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
+    config.add_route('article', '/article/{id}')
+
+    config.add_route('admin_login', '/ffefer30vaslfsflvalfje/admin/login')
+    config.add_route('admin_logout', '/ffefer30vaslfsflvalfje/admin/logout')
+    config.add_route('admin_home', '/ffefer30vaslfsflvalfje/admin')
+
+    config.add_route('admin_users', '/ffefer30vaslfsflvalfje/admin/users')
+    config.add_route('admin_user_edit', '/ffefer30vaslfsflvalfje/admin/user/{id}/edit')
+    config.add_route('admin_user_add', '/ffefer30vaslfsflvalfje/admin/user/add')
+
+    config.add_route('admin_articles', '/ffefer30vaslfsflvalfje/admin/articles')
+    config.add_route('admin_article_edit', '/ffefer30vaslfsflvalfje/admin/article/{id}/edit')
+    config.add_route('admin_article_add', '/ffefer30vaslfsflvalfje/admin/article/add')
+
     config.scan()
     return config.make_wsgi_app()
--- blogweb/models.py
+++ blogweb/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    DateTime
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -17,11 +18,23 @@ from zope.sqlalchemy import ZopeTransactionExtension
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
-
-class MyModel(Base):
-    __tablename__ = 'models'
+class Article(Base):
+    __tablename__ = 'articles'
     id = Column(Integer, primary_key=True)
-    name = Column(Text)
-    value = Column(Integer)
+    title = Column(String)
+    body = Column(Text)
+    author = Column(String)
+    create_dt = Column(DateTime)
+    update_dt = Column(DateTime)
 
-Index('my_index', MyModel.name, unique=True, mysql_length=255)
+Index('article_index', Article.id, unique=True, mysql_length=255)
+
+class AdminUser(Base):
+    __tablename__ = 'admin_users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    password = Column(String)
+    create_dt = Column(DateTime)
+    update_dt = Column(DateTime)
+
+Index('admin_user_index', AdminUser.id, unique=True, mysql_length=255)
