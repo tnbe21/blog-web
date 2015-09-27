c458cb4b4ce26e7e2758a2c7ce3f39618e9b6c05
tanabe_taichi <whpbj809@gmail.com>
2015-09-28 02:07:24 +0900

    update packaging admin.article

--- blogweb/views/admin/article.py
+++ blogweb/views/admin/article.py
@@ -6,15 +6,13 @@ from pyramid.httpexceptions import HTTPFound
 from ...models import DBSession
 from ...models.article import Article
 
-@view_config(route_name='admin_articles', renderer='blogweb:templates/admin/articles.pt')
+@view_config(route_name='admin_articles', renderer='blogweb:templates/admin/article/index.pt')
 def index(request):
     return {}
 
-@view_config(route_name='admin_article_edit_view', renderer='blogweb:templates/admin/article_edit.pt')
-def edit_view(request):
-    article_id = request.params.get('article_id')
-    article = DBSession.query(Article).filter_by(article_id=article_id).first()
-    return dict(article=article)
+@view_config(route_name='admin_article_add_view', renderer='blogweb:templates/admin/article/add.pt')
+def add_view(request):
+    return {}
 
 @view_config(route_name='admin_article_add')
 def add(request):
@@ -26,6 +24,12 @@ def add(request):
         article.status = request.params.get('status')
         DBSession.add(article)
         return HTTPFound(location='/rfwt4w3gtibjqhaljgalkjkl30va/admin/articles')
+
+@view_config(route_name='admin_article_edit_view', renderer='blogweb:templates/admin/article/edit.pt')
+def edit_view(request):
+    article_id = request.params.get('article_id')
+    article = DBSession.query(Article).filter_by(article_id=article_id).first()
+    return dict(article=article)
 
 @view_config(route_name='admin_article_edit')
 def edit(request):
